fc87f42c0c245aaca5313007c36cee37b62ef13e
Việt Trần Quốc <binhviet234@outlook.com>
2025-04-15 20:14:08 +0700

    Initial commit

---
+++ WebAppYte/Controllers/CaKhamController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppYte.DAO;
+using WebAppYte.Models;
+using WebAppYte.Common;
+using PagedList;
+
+namespace WebAppYte.Controllers
+{
+    public class CaKhamController : Controller
+    {
+        private modelWeb db = new modelWeb();
+
+        // GET: CaKham
+
+        public ActionResult Index(int id, int? page)
+        {
+
+            var lich = db.CaKhams.Where(x => x.mand == id && x.trangthai == 1).OrderByDescending(x => x.ngaykham).ThenBy(x => x.maca).ToList();
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(lich.ToPagedList(pageNumber, pageSize));
+        }
+        public ActionResult Create(int? id)
+        {
+            ViewBag.mand = new SelectList(db.NguoiDungs.Where(h => h.mand == id), "mand", "hoten");
+
+            return View();
+        }
+
+        // POST: Admin/Tintucs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "maca, ngaykham, hinhthuc, ca, mand")] CaKham cakham)
+        {
+            if (ModelState.IsValid)
+            {
+                cakham.trangthai = 1;
+                db.CaKhams.Add(cakham);
+                db.SaveChanges();
+                return RedirectToAction("Index", "CaKham", new { id = cakham.mand });
+
+            }
+            ViewBag.mand = new SelectList(db.NguoiDungs, "mand", "hoten", cakham.mand);
+            return View(cakham);
+        }
+
+        // GET: Admin/Tintucs/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CaKham cakham = db.CaKhams.Find(id);
+            if (cakham == null)
+            {
+                return HttpNotFound();
+            }
+            @ViewBag.ngay = (cakham.ngaykham).Value.ToString("yyyy-MM-dd");
+
+            return View(cakham);
+        }
+
+        // POST: Admin/Tintucs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(CaKham model)
+        {
+            if (ModelState.IsValid)
+            {
+                var userBS = Session["userBS"] as WebAppYte.Models.NguoiDung;
+                CaKham cakham = db.CaKhams.Find(model.maca);
+                if (cakham == null) return HttpNotFound();
+
+
+                cakham.hinhthuc = model.hinhthuc;
+                cakham.ngaykham = model.ngaykham;
+                cakham.ca = model.ca;
+                cakham.trangthai = 1;
+                cakham.mand = userBS.mand;
+
+                db.Entry(cakham).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index", new { id = userBS.mand });
+            }
+            return View(model);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CaKham cakham = db.CaKhams.Find(id);
+            if (cakham == null)
+            {
+                return HttpNotFound();
+            }
+            @ViewBag.ngay = (cakham.ngaykham).Value.ToString("yyyy-MM-dd");
+
+            return View(cakham);
+        }
+
+        // POST: Admin/Tintucs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Delete(CaKham model)
+        {
+            if (ModelState.IsValid)
+            {
+                var cakham = db.CaKhams.Find(model.maca);
+                if (cakham == null) return HttpNotFound();
+
+                cakham.trangthai = 0;
+                db.Entry(cakham).State = EntityState.Modified;
+                db.SaveChanges();
+
+
+                return RedirectToAction("Index", new { id = cakham.mand });
+            }
+
+            return View(model);
+        }
+        // GET: Admin/Tintucs/Delete/5
+
+
+        // POST: Admin/Tintucs/Delete/5
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebAppYte/Controllers/HomeController.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppYte.Models;
+using PagedList;
+using WebAppYte.Common;
+using WebAppYte.DAO;
+using System.Web.Helpers;
+using System.Configuration;
+using System.Net.Mail;
+using System.Data.Entity.Validation;
+namespace WebAppYte.Controllers
+{
+    public class HomeController : Controller
+    {
+        modelWeb db = new modelWeb();
+       
+        public ActionResult index()
+        {
+            return View();
+
+        }
+        
+        public ActionResult Trangchu()
+        {
+            return View();
+
+        }
+
+        [HttpGet]
+        public ActionResult Dangky()
+        {
+            ViewBag.gioitinh = new string[] { "Nam", "Nữ" };
+            return View();
+        }
+
+        // POST: Admin/NguoiDungs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Dangky([Bind(Include = "mabn,tenbn,sdt,email,diachi,ngaysinh, gioitinh, tendn,mk")] BenhNhan benhnhan)
+        {
+            if (ModelState.IsValid)
+            {
+                int check = db.BenhNhans.Where(x => x.tendn == benhnhan.tendn).Count();
+                if (check == 0)
+                {
+                    db.BenhNhans.Add(benhnhan);
+                    db.SaveChanges();
+                    Session["benhnhan"] = benhnhan;
+                    return RedirectToAction("Dangnhap");
+                }
+                else
+                {
+                    ViewBag.gioitinh = new string[] { "Nam", "Nữ" };
+                    ViewBag.loi = "Tên đăng nhập đã tồn tại!";
+                    return View(benhnhan);
+                }
+            }
+
+            ViewBag.gioitinh = new string[] { "Nam", "Nữ" };
+            return View(benhnhan);
+        }
+    
+        [HttpGet]
+        public ActionResult Dangnhap()
+        {
+            return View();
+        }
+        public ActionResult Gioithieu()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Dangnhap(FormCollection Dangnhap)
+        {
+            string tk = Dangnhap["tendn"].ToString();
+            string mk = Dangnhap["mk"].ToString();
+            var islogin = db.BenhNhans.Where(x=>x.trangthai==1).SingleOrDefault(x => x.tendn.Equals(tk) && x.mk.Equals(mk));
+            var isloginAdmin = db.NguoiDungs.Where(x => x.trangthai == 1).SingleOrDefault(x => x.tendn.Equals(tk) && x.mk.Equals(mk));
+            if (islogin != null)
+            {
+                Session["user"] = islogin;
+                return RedirectToAction("Details", "BenhNhan", new { id = @islogin.mabn });
+              
+            }
+            else if (isloginAdmin != null && isloginAdmin.quyen == 0)
+            {
+                Session["userAdmin"] = isloginAdmin;
+                return RedirectToAction("QuanTris", "Admin");
+            }
+            else if (isloginAdmin != null && isloginAdmin.quyen == 1)
+            {
+                 Session["userBS"] = isloginAdmin;
+                return RedirectToAction("Trangchu", "Home");
+            }
+            else
+            {
+                ViewBag.Fail = "Tài khoản hoặc mật khẩu không chính xác.";
+                return View("Dangnhap");
+            }
+
+        }
+        public class listofSegments
+        {
+            public string Text { get; set; }
+            public string Value { get; set; }
+        }
+        
+
+        public ActionResult DangXuat()
+        {
+            Session["user"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult DangXuatBs()
+        {
+            Session["userBS"] = null;
+            return RedirectToAction("Index", "Home");
+        }
+		[HttpGet]
+		public ActionResult QuenMatKhau()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult QuenMatKhau(string email)
+		{
+			if (string.IsNullOrEmpty(email))
+			{
+				ViewBag.Message = "Vui lòng nhập email đã đăng ký";
+				return View();
+			}
+
+			// Kiểm tra trong bảng bệnh nhân
+			var benhNhan = db.BenhNhans.FirstOrDefault(a => a.email == email);
+			// Kiểm tra trong bảng người dùng (admin/bs)
+			var nguoiDung = db.NguoiDungs.FirstOrDefault(a => a.email == email);
+
+			if (benhNhan == null && nguoiDung == null)
+			{
+				ViewBag.Message = "Email không tồn tại trong hệ thống";
+				return View();
+			}
+
+			// Tạo mã reset và thời hạn
+			var resetCode = Guid.NewGuid().ToString();
+			var expiry = DateTime.Now.AddHours(1);
+
+			if (benhNhan != null)
+			{
+				benhNhan.ResetPasswordCode = resetCode;
+				benhNhan.ResetPasswordCodeExpiry = expiry;
+			}
+			else if (nguoiDung != null)
+			{
+				nguoiDung.ResetPasswordCode = resetCode;
+				nguoiDung.ResetPasswordCodeExpiry = expiry;
+			}
+
+			try
+			{
+				db.SaveChanges();
+			}
+			catch (DbEntityValidationException ex)
+			{
+				string errorMsg = "";
+				foreach (var validationErrors in ex.EntityValidationErrors)
+				{
+					foreach (var validationError in validationErrors.ValidationErrors)
+					{
+						errorMsg += $"Thuộc tính: {validationError.PropertyName} - Lỗi: {validationError.ErrorMessage}<br />";
+					}
+				}
+				ViewBag.Message = "Lỗi khi lưu dữ liệu: <br />" + errorMsg;
+				return View();
+			}
+
+			// Gửi email
+			SendResetPasswordEmail(email, resetCode);
+
+			ViewBag.Message = "Hướng dẫn khôi phục mật khẩu đã được gửi đến email của bạn";
+			return View();
+		}
+
+
+		[HttpGet]
+		public ActionResult DatLaiMatKhau(string code)
+		{
+			if (string.IsNullOrEmpty(code))
+			{
+				return HttpNotFound();
+			}
+
+			// Kiểm tra trong cả 2 bảng
+			var benhNhan = db.BenhNhans.FirstOrDefault(a => a.ResetPasswordCode == code);
+			var nguoiDung = db.NguoiDungs.FirstOrDefault(a => a.ResetPasswordCode == code);
+
+			if ((benhNhan == null || benhNhan.ResetPasswordCodeExpiry < DateTime.Now) &&
+				(nguoiDung == null || nguoiDung.ResetPasswordCodeExpiry < DateTime.Now))
+			{
+				ViewBag.Message = "Mã khôi phục không hợp lệ hoặc đã hết hạn";
+				return View("LienKetKhongHopLe");
+			}
+
+			ViewBag.ResetCode = code;
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult DatLaiMatKhau(string code, string newPassword)
+		{
+			// Kiểm tra trong bảng bệnh nhân
+			var benhNhan = db.BenhNhans.FirstOrDefault(a => a.ResetPasswordCode == code);
+			// Kiểm tra trong bảng người dùng
+			var nguoiDung = db.NguoiDungs.FirstOrDefault(a => a.ResetPasswordCode == code);
+
+			if ((benhNhan != null && benhNhan.ResetPasswordCodeExpiry >= DateTime.Now) ||
+				(nguoiDung != null && nguoiDung.ResetPasswordCodeExpiry >= DateTime.Now))
+			{
+				if (benhNhan != null)
+				{
+					benhNhan.mk = newPassword; // Nên mã hóa mật khẩu
+					benhNhan.ResetPasswordCode = null;
+					benhNhan.ResetPasswordCodeExpiry = null;
+				}
+				else if (nguoiDung != null)
+				{
+					nguoiDung.mk = newPassword; // Nên mã hóa mật khẩu
+					nguoiDung.ResetPasswordCode = null;
+					nguoiDung.ResetPasswordCodeExpiry = null;
+				}
+
+				db.SaveChanges();
+
+				ViewBag.Message = "Mật khẩu đã được đặt lại thành công";
+				return View("DatLaiMatKhauThanhCong");
+			}
+
+			ViewBag.Message = "Mã khôi phục không hợp lệ hoặc đã hết hạn";
+			return View("LienKetKhongHopLe");
+		}
+
+		private void SendResetPasswordEmail(string emailAddress, string resetCode)
+		{
+			try
+			{
+				var resetLink = Url.Action("DatLaiMatKhau", "Home",
+					new { code = resetCode }, protocol: Request.Url.Scheme);
+
+				var subject = "Khôi phục mật khẩu - Hệ thống Y tế";
+				var body = $"Xin chào,<br/><br/>Chúng tôi nhận được yêu cầu khôi phục mật khẩu của bạn. " +
+					"Vui lòng nhấp vào liên kết sau để đặt lại mật khẩu:<br/><br/>" +
+					$"<a href='{resetLink}'>{resetLink}</a><br/><br/>" +
+					"Liên kết này sẽ hết hạn sau 1 giờ.<br/><br/>" +
+					"Nếu bạn không yêu cầu khôi phục mật khẩu, vui lòng bỏ qua email này.";
+
+				// Lấy display name từ web.config
+				var displayName = ConfigurationManager.AppSettings["MailDisplayName"] ?? "Hệ thống Y tế";
+
+				using (var message = new MailMessage())
+				{
+					message.From = new MailAddress("ducduong.tektra@gmail.com", displayName);
+					message.To.Add(emailAddress);
+					message.Subject = subject;
+					message.Body = body;
+					message.IsBodyHtml = true;
+
+					using (var client = new SmtpClient())
+					{
+						// Các thông số sẽ tự động lấy từ web.config
+						client.Send(message);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				// Ghi log lỗi
+				System.Diagnostics.Debug.WriteLine($"Lỗi gửi email: {ex.Message}");
+			}
+		}
+
+		public ActionResult DatLaiMatKhauThanhCong()
+		{
+			return View();
+		}
+
+		public ActionResult LienKetKhongHopLe()
+		{
+			return View();
+		}
+
+	}
+}
